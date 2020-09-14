9368bd6cec39c28e1cbaec3838991dc50be3437c
FilipBuj <filip.bujakowski@gds.ey.com>
2020-09-14 10:33:00 +0200

    Secend Init Dev Commit

--- VirtualHome/Program.cs
+++ VirtualHome/Program.cs
@@ -6,7 +6,7 @@ namespace VirtualHome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! bb");
+            Console.WriteLine("Hello World! aabb");
         }
     }
 }
