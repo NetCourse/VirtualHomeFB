ddda5ce301ffc2a5ed55b3c2c0a16329df90e8e1
FilipBuj <filip.bujakowski@gds.ey.com>
2020-09-17 19:16:12 +0200

    Created: RoomList + Romm

---
+++ VirtualHome/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualHome
+{
+    class Room
+    {
+        public string Name { get; set; }
+
+        public RoomList Type { get; set; }
+
+        public Room(string name, RoomList type)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+}
---
+++ VirtualHome/RoomList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualHome
+{
+    enum RoomList
+    {
+         Kitchen = 0,
+         Hallway = 1,
+         Bathroom = 2,
+         LivingRoom = 3,
+         Bedroom = 4
+    }
+}
